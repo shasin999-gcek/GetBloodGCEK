bf390e0a577b01b18c1f06beb9afa9aae0424779
Muhammed Shasin P <mohdshasin313@gmail.com>
2020-03-26 23:15:00 +0530

    Passport Authorisation Added

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('permissions')->insert([
+            ['type' => 'ALL(Add, Edit, View)'],
+            ['type' => 'View-Only']
+        ]);
+    }
+}
